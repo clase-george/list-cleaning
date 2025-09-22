b9dd1a6c88d37394cf7400639bea1d9486f9e96d
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-09-22 19:50:26 +0200

    initial commit

---
+++ src/main/java/es/fpsumma/App.java
@@ -0,0 +1,13 @@
+package es.fpsumma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
