d2ff585d870c40c28e3e5a6ac20e741e566f6631
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-09-25 20:39:50 +0200

    agregada funcion eliminarDuplicados

--- src/main/java/es/fpsumma/App.java
+++ src/main/java/es/fpsumma/App.java
@@ -15,15 +15,26 @@ public class App
             new Asistente("Óscar", 40, "oscar@mail.es")
         ));
 
+        //Mostramos los nombres limpios, este metodo solo modifica impresion pantalla
         for(Asistente a : asistentes){
             String nombreLimpio = Asistente.nombreLimpio(a.nombre());
             String primerUpperCase = Asistente.primeraUpperCase(nombreLimpio);
             System.out.println(primerUpperCase);
         }
+        //Mostramos solo los asistentes con email valido, este modifica la lista en si
         Asistente.emailValido(asistentes);
         System.out.println("Asistentes con email valido:");
         for(Asistente a : asistentes){
             System.out.println(a);
+        }
+
+        //MOstamos nombre limpio y correo valido correos sin duplicar, este modifica la lista en si
+        Asistente.eliminarDuplicados(asistentes);
+        System.out.println("Asistentes sin duplicados:");
+        for(Asistente a : asistentes){
+            String nombreLimpio = Asistente.nombreLimpio(a.nombre());
+            String primerUpperCase = Asistente.primeraUpperCase(nombreLimpio);
+            System.out.println(primerUpperCase + " - " + a.email());
         }
     }
 }
--- src/main/java/es/fpsumma/Records.java
+++ src/main/java/es/fpsumma/Records.java
@@ -10,8 +10,8 @@ public record Records() {
             return nombre.trim().toLowerCase();
         }
         public static String primeraUpperCase(String nombre){
-            nombre = Character.toUpperCase(nombre.charAt(0)) + nombre.substring(1); //dividimos el nombre en caracteres, hacemos que la posicion 0 se aplique el uppercase y luego le concatenamos los caracteres a partir de la primera con el substring
-            return nombre;
+            //return Character.toUpperCase(nombre.charAt(0)) + nombre.substring(1); //dividimos el nombre en caracteres, hacemos que la posicion 0 se aplique el uppercase y luego le concatenamos los caracteres a partir de la primera con el substring
+            return nombre.substring(0,1).toUpperCase() + nombre.substring(1); //otra forma de hacerlo, con substring tomamos el primer caracter y le aplicamos el uppercase y luego le concatenamos el resto del string a partir de la primera posicion
         }
         public static void emailValido(List<Asistente> asistentes){
             String emailRegex = "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$";  //este regex indica que debe haber 1 o mas caracteres que no sean @, luego una @, luego nuevamente lo primero, luego un . literal y otrra vez lo primero
@@ -20,6 +20,10 @@ public record Records() {
             asistente.email().trim().isEmpty() || //verificamos que si quitamos los espacios(trim) no quede vacio
             !verificador.matcher(asistente.email()).matches()); //verificamos con matcher que el email cumple el patron del regex, si no lo cumple lo eliminamos
         }
+        public static void eliminarDuplicados(List<Asistente> asistentes){
+            Set<String> vistos = new HashSet<>(); //creamos un set para almacenar los nombres que ya hemos visto, el set no permite duplicados
+            asistentes.removeIf(asistente-> !vistos.add(asistente.email())); //En el lambda puedo poner lo que sea, se sobre entiende que es un elemento de la lista
+        }                                                                    //se borrara de la lista el elemento asistente que no se añada al Hash ya que eso significa que esta repetido
     }
     public record Artista(String nombre, Set<String> generos) {}
     public record Actuacion(String artista, String escenario, LocalTime inicio, LocalTime fin, int aforoMax) {}
